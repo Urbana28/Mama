0be47f71d79df485531417fccc5bfd3b5481fc0c
Artem Kotov <a.kotov@nord-soft.com>
2020-06-04 18:54:54 +0300

    first commit

---
+++ src/components/login/Login.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="loginContainer">
+    <div class="loginContainer__filed"><input type="text"></div>
+    <div class="loginContainer__filed"><input type="password"></div>
+    blala
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+  name: 'Login',
+  components: {
+  }
+}
+</script>
+
+
+<style scoped lang="scss">
+    .loginContainer {
+
+      &__field {
+
+      }
+    }
+</style>
